aeb08ea3b3acc5861ffa63915cc811630d9221ae
Garrett Finucane <garrettdreyfus@gmail.comm>
2019-02-02 15:49:34 -0800

    turned on database storage

--- public/scripts/mldidentifier.js
+++ public/scripts/mldidentifier.js
@@ -164,10 +164,10 @@ document.getElementById("densityChart").onclick = function (evt) {
 	var points = dx.getElementsAtEvent(evt);
 	console.log(points);
 	if(points[0]) {
-		//answers.push({"depth":profile["pressures"][points[0]._index],"id":profile.name});
-		//postData('https://hidden-brook-86362.herokuapp.com/mld/create',{"profileName":profile.name,"depth":profile["pressures"][points[0]._index],"identifierName":localStorage.getItem("name")})
-		//.then(data => console.log(data)) // JSON-string from `response.json()` call
-		//.catch(error => console.error(error));
+		answers.push({"depth":profile["pressures"][points[0]._index],"id":profile.name});
+		postData('https://hidden-brook-86362.herokuapp.com/mld/create',{"profileName":profile.name,"depth":profile["pressures"][points[0]._index],"identifierName":localStorage.getItem("name")})
+		.then(data => console.log(data)) // JSON-string from `response.json()` call
+		.catch(error => console.error(error));
 	profiles.splice(profileNumber,1)
 		localStorage.setItem("data",JSON.stringify(profiles));
 		updateGraph();
